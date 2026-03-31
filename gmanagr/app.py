1cdd05712ffa9ab5453de34d87348e8e7e0259e2
16ur <akuseruuu@protonmail.com>
2026-03-31 21:55:05 +0200

    feat(datatable): add datatable with primary elements

--- gmanagr/app.py
+++ gmanagr/app.py
@@ -1,14 +1,26 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.widgets import Footer, Header, DataTable
+from gmanagr.auth import checkToken
+from gmanagr.gmail_client import GmailClient, Email
 
 
 class Gmanagr(App):
     THEME = "catppuccin-mocha"
     BINDINGS = [("d", "toggle_dark", "change theme")]
     
-    def on_mount(self) -> None:
+    async def on_mount(self) -> None:
         self.theme = "catppuccin-mocha"
+        table = self.query_one(DataTable)
+        table.add_columns("Date", "From", "Subject")
+        
+        creds = checkToken()
+        client = GmailClient(creds)
+        mails = client.get_messages(1)
+        
+        for mail in mails:
+            table.add_row(mail.date, mail.from_raw, mail.subject)
     
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
+        yield DataTable()
